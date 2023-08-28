396130715dcfed959f5348082b143375087e3110
Rio on Mac <riochr17@gmail.com>
2023-08-28 14:58:01 +0700

    update

--- src/app-specs/test-server.ts
+++ src/app-specs/test-server.ts
@@ -2,10 +2,10 @@ import { Register } from "./backend-specs/account-auth.design";
 import { MyCertificate } from "./backend-specs/account-data.design";
 import { BackendSystem } from "./backend-system.generated";
 
-const registerLogic: BackendSystem.Logic.IRegister = async (param: { body: Register.Body }): Promise<Register.Output> => {
-  console.log(param);
-  return true;
-}
+// const registerLogic: BackendSystem.Logic.IRegister = async (param: { body: Register.Body }): Promise<Register.Output> => {
+//   console.log(param);
+//   return true;
+// }
 
 const myCertificateLogic: BackendSystem.Logic.IMyCertificate = async (param: { query: MyCertificate.Query, header: MyCertificate.Header,  }): Promise<MyCertificate.Output> => {
   console.log(param);
@@ -17,7 +17,7 @@ const myCertificateLogic: BackendSystem.Logic.IMyCertificate = async (param: { q
 
 new BackendSystem.Engine().init({ port: 8000 })
 .then((engine: BackendSystem.Engine) => {
-  engine.register(registerLogic);
+  // engine.register(registerLogic);
   engine.myCertificate(myCertificateLogic);
 })
 .catch((error: any) => {
